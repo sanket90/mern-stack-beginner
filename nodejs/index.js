e8a5da217084cfe85fc80ba6878ba924b0a5830a
sanket90 <sanket.upadhyay@infoud.co.in>
2021-11-21 10:32:38 +0530

    [Add] nodeJs requests, module, params, Request js integration

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,14 +1,47 @@
 import { createServer } from 'http';
-
-
+import HotelDataService from './hotel-service.js'
+import { parse as urlParse } from 'url';
+import { parse as queryParse } from 'querystring';
 
 createServer((request, response) => {
-    console.log("METHOD", request.method);
-    console.log("URL", request.url);
-    console.log("HEADERS", request.headers);
+    console.log("METHOD", request.method, "URL", request.url);
 
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end("Coming Soon..!");
+    const url = request.url;
+
+    response.writeHead(200, { 
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+    });
+
+    // GET HOTEL LIST
+    if (url == '/fetchAllHotels') {
+        const service = new HotelDataService();
+        service.getAllHotels(data => {
+            response.write(data);
+            response.end();
+        });
+    }
+
+    // GET HOTEL BY ID
+    else if (url.startsWith('/fetchHotelById')) {
+        const parsedUrlQuery = urlParse(url).query;
+        const parsedParms = queryParse(parsedUrlQuery);
+        const hotelId = parsedParms.id;
+        
+        const service = new HotelDataService();
+        service.getHotelById(hotelId, data => {
+            response.write(data);
+            response.end();
+        });
+    }
+    
+    // GET DEFAULT RESPONSE
+    else {
+        response.write("Coming Soon..!");
+        response.end();
+    }
+
+
 }).listen(8000, () => {
     console.log("Listing on port 8000");
 });
--- reactjsapp/src/data/HotelDataService.js
+++ reactjsapp/src/data/HotelDataService.js
@@ -2,56 +2,13 @@ import axios from 'axios';
 
 class HotelDataService {
 
-    hotelList = [
-        {
-          "id": 1234,
-          "title": "Taj Hotel",
-          "brand": "TATA",
-          "description": "This is best hotel ever.!",
-          "location": "Delhi",
-          "currentPrice": 50000,
-          "originalPrice": 70000,
-          "imgUrl": ""
-        },
-        {
-          "id": 5678,
-          "title": "Sahara Star",
-          "brand": "Sahara",
-          "description": "This is worst hotel ever.!",
-          "location": "Mumbai",
-          "currentPrice": 500,
-          "originalPrice": 700,
-          "imgUrl": ""
-        },
-        {
-          "id": 4321,
-          "title": "Taj Hotel",
-          "brand": "TATA",
-          "description": "This is best hotel ever.!",
-          "location": "Mumbai",
-          "currentPrice": 150000,
-          "originalPrice": 170000,
-          "imgUrl": ""
-        },
-        {
-          "id": 8765,
-          "title": "Sahara Star",
-          "brand": "Sahara",
-          "description": "This is worst hotel ever.!",
-          "location": "Delhi",
-          "currentPrice": 1500,
-          "originalPrice": 7000,
-          "imgUrl": ""
-        }
-    ];
-
     sleep() {
       return new Promise(resolve => setTimeout(resolve, 2000))
     }
 
     async getAllHotels() {
         await this.sleep()
-        const response = await axios.get("/data/productList.json")
+        const response = await axios.get("http://localhost:8000/fetchAllHotels")
         const hotelList = response.data;
         
         return hotelList.sort((item1, item2) => item2.currentPrice - item1.currentPrice);
@@ -59,10 +16,9 @@ class HotelDataService {
 
     async getHotelById(id) {
       await this.sleep()
-      const response = await axios.get("/data/productList.json")
-      const hotelList = response.data;
-      
-      return hotelList.find(item => item.id == id);
+      const response = await axios.get(`http://localhost:8000/fetchHotelById?id=${id}`)
+      const hotelObject = response.data;
+      return hotelObject;
     }
 
     filterHotelByName(title) {
